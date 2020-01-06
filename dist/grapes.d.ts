dee78a452cff5d82e5725e6154562679bceea47a
Sam Broner <sabroner@microsoft.com>
2020-01-06 15:20:25 -0800

    some basic types

--- dist/grapes.d.ts
+++ dist/grapes.d.ts
@@ -12,10 +12,24 @@ export interface IEditorConfig  extends IEditorViewConfig {
     container: string | HTMLElement;
     components?: any;
     autorender?: boolean;
-    plugins?: Plugin[];
+    plugins?: PluginFn[];
     pluginsOpts?: any;
     storageManager?: any;
-    blockManager?: any;
+    blockManager?: {
+        appendTo: HTMLElement;
+        blocks: ({
+            id: string;
+            label?: string;
+            attributes?: {
+                class: string;
+            };
+            select?: boolean;
+            content: string | {
+                type: string;
+            };
+            activate?: boolean;
+        })[];
+    };
     panels?: any;
     [pluginId: string]: IPluginOptions;
 }
@@ -23,41 +37,45 @@ export interface IEditorConfig  extends IEditorViewConfig {
 export interface IEditorViewConfig {
     height?: string;
     width?: string;
-    stylePrefix?: any;
     el?: HTMLElement;
     cssIcons?: any;
 }
 
-export interface IEditor {
+export interface IEditor extends Backbone.Model {
     $: any;
     editor: IEditorModel;
     I18n: any;
-    DomComponents: any;
     LayerManager: any;
     CssComposer: any;
     StorageManager: any;
+    DomComponents: IDomComponents;
     AssetManager: any;
     BlockManager: any;
     TraitManager: any;
     SelectorManager: any;
 
     init(): this;
-
     getHtml(opts: any): string;
     getComponents(): any;
     getWrapper();
-    addComponents(components: any, opts?: {
+    /**
+     * 
+     * @param components HTML String or Component Model
+     * @param opts 
+     */
+    addComponents(components: Array<Object> | Object | string, opts?: {
         avoidUpdateStyle?: boolean;
     }): any[];
-
     setStyle(style: any): any;
-
     getContainer(): HTMLElement;
-
     refresh(): void
 }
 
 export interface IEditorModel extends Backbone.Model {
+
+}
+
+export interface IBlockManager {
 
 }
 
@@ -68,13 +86,48 @@ export interface IEditorView extends Backbone.View {
 }
 
 export interface IPluginManager {
-    add(id: string, plugin: Plugin): Plugin;
+    add(id: string, plugin: PluginFn): PluginFn;
 
-    get(id: string): Plugin;
+    get(id: string): PluginFn;
 }
 
-export type Plugin = (editor: IEditor, plgOptions: any)=> any;
+export type PluginFn = (editor: IEditor, plgOptions?: any) => any;
 
 export interface IPluginOptions {
 
+}
+
+export interface IDomComponents {
+
+    addType(type: string, methods: IComponentMethods): any;
+    load(data?: any): any;
+    store(noStore: boolean): any;
+    addComponent(component: IComponent): IComponent | IComponent[];
+}
+
+export interface IComponent {
+    tagName?: string;
+    type?: string;
+    removable?: boolean;
+    draggable?: boolean;
+    droppable?: boolean;
+    badgable?: boolean;
+    stylable?: boolean;
+    copyable?: boolean;
+    content?: string;
+    style?: any;
+    attributes?: any;
+    traits?: any[];
+
+    is?(type: string): boolean;
+}
+
+export interface IComponentMethods {
+    isComponent: any;
+    model?: {
+        defaults: IComponent;
+    }
+    view?: any;
+    extend?: any;
+    extendFn?: any;
 }
